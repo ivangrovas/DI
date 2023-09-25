bf58f6678e385bd8026a8fae20706ccb1dffe4f8
Ivan Grovas <igrovasp22@fpcoruna.afundacion.org>
2023-09-25 12:27:32 +0200

    Completada tarea 1, adivinanza.py

---
+++ sprint0python/adivinanza.py
@@ -0,0 +1,17 @@
+opcion_correcta = "A"
+opcion = " "
+
+while (opcion!=opcion_correcta):
+
+    print ("El roer es mi trabajo,el queso mi aperitivo y el gato ha sido siempre mi más temido enemigo.")
+    print ("\nOpciones : A)Ratón B)Gato C)Tigre")
+    opcion = input ("\n\tTeclea opción? ")
+
+    if (opcion==opcion_correcta):
+        print ("\nFELICIDADES. RESPUESTA CORRECTA !!!!")
+
+    if (opcion!=opcion_correcta):
+        print(" La opción es incorrecta, inténtalo de nuevo. ")
+
+
+
