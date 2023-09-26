99d06f3711cceb495cd01208f2fa866cad0996b0
Ivan Grovas <igrovasp22@fpcoruna.afundacion.org>
2023-09-26 09:44:26 +0200

    Completada tarea 2, ampliación adivinanza.py

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,17 +1,84 @@
-opcion_correcta = "A"
+
+opcion = " "
+contAciertos = 0
+opcionMayus=" "
+
+adivinanzas = {
+    1 : "El roer es mi trabajo,el queso mi aperitivo y el gato ha sido siempre mi más temido enemigo.", 
+    2 : "Me pisas y no me quejo, me cepillas si me mancho, y con mi hermano gemelo, bajo tu cama descanso.",
+    3 : "Oro parece, plata no es. Abran las cortinas y verán lo que es.",
+}
+
+respuestas = {
+    1: "A) Ratón, B) Rata, C) Perro",
+    2: "A) Calcetín, B) Zapato, C) Camiseta",
+    3: "A) Naranja, B) Platano, C) Fresa"
+}
+
+correctas = {
+   1:"A",
+   2:"B",
+   3:"B"
+}
+
+print(adivinanzas[1])
+print(respuestas[1])
+opcion = input ("\n\tTeclea opción? ")
+opcionMayus = opcion.upper()
+
+if (opcionMayus==correctas[1]):
+    print ("\nFELICIDADES. RESPUESTA CORRECTA !!!!")
+    contAciertos = contAciertos +10;
+
+if (opcionMayus!=correctas[1]):
+    print(" La opción es incorrecta, has fallado. ")
+    contAciertos = contAciertos -5;
+
 opcion = " "
 
-while (opcion!=opcion_correcta):
+print(adivinanzas[2])
+print(respuestas[2])
+opcion = input ("\n\tTeclea opción? ")
+opcionMayus = opcion.upper();
 
-    print ("El roer es mi trabajo,el queso mi aperitivo y el gato ha sido siempre mi más temido enemigo.")
-    print ("\nOpciones : A)Ratón B)Gato C)Tigre")
-    opcion = input ("\n\tTeclea opción? ")
+print(adivinanzas[2])
+print(respuestas[2])
 
-    if (opcion==opcion_correcta):
-        print ("\nFELICIDADES. RESPUESTA CORRECTA !!!!")
+if (opcionMayus==correctas[2]):
+    print ("\nFELICIDADES. RESPUESTA CORRECTA !!!!")
+    contAciertos = contAciertos +10;
 
-    if (opcion!=opcion_correcta):
-        print(" La opción es incorrecta, inténtalo de nuevo. ")
+if (opcionMayus!=correctas[2]):
+    print(" La opción es incorrecta, has fallado. ")
+    contAciertos = contAciertos -5;
+
+opcion = " "
+opcionMayus= " "
+
+print(adivinanzas[3])
+print(respuestas[3])
+
+opcion = input ("\n\tTeclea opción? ")
+opcionMayus = opcion.upper();
+
+print(adivinanzas[3])
+print(respuestas[3])
+
+if (opcionMayus==correctas[3]):
+    print ("\nFELICIDADES. RESPUESTA CORRECTA !!!!")
+    contAciertos = contAciertos +10;
+
+if (opcionMayus!=correctas[3]):
+    print(" La opción es incorrecta, has fallado. ")
+    contAciertos = contAciertos -5;
+
+print ("TU PUNTUACIÓN HA SIDO: ")
+print (contAciertos)
+
+
+
+
+
 
 
 
