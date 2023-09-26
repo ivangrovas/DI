156cb89ee5ec59e9c5fd208641b2a80da7de9810
Ivan Grovas <igrovasp22@fpcoruna.afundacion.org>
2023-09-26 10:12:05 +0200

    Completada tarea 3, operaciones.py

---
+++ sprint0python/operaciones.py
@@ -0,0 +1,17 @@
+a = 0
+b = 0
+
+def suma_de_dos_numeros(a, b):
+    return a + b
+
+def resta_de_dos_numeros(a, b):
+    return a - b
+
+def multiplicacion_de_dos_numeros (a, b):
+    return a * b
+
+def division_de_dos_numeros(a, b):
+    if (b == 0): 
+        print ("ERROR: No se puede dividir entre 0")
+    else:
+        return a / b
