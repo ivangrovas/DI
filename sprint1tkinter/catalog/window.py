f646616bea7dca40c8e0c946c718cb8ef43d095f
Ivan Grovas <igrovasp22@fpcoruna.afundacion.org>
2023-10-09 11:57:01 +0200

    Completada tarea 6

--- sprint1tkinter/catalog/window.py
+++ sprint1tkinter/catalog/window.py
@@ -3,6 +3,7 @@ import tkinter as tk
 #from yes_window import show_yes_window
 #from no_window  import show_no_window
 from cell import Cell
+from PIL import Image, ImageTk
 
 class MainWindow:
    
@@ -16,14 +17,18 @@ class MainWindow:
 
         #Inicializamos 5 celdas
         self.cells = [
-            Cell("F1 1", "C:\\msys64\\home\\Usuario\\DI\\sprint1tkinter\\catalog\\data\\edited\\mvwdc.jpeg"),
-            Cell("F1 2", "C:\\msys64\\home\\Usuario\\DI\\sprint1tkinter\\catalog\\data\\edited\\th-828554261.jpeg"),
-            Cell("F1 3", "C:\\msys64\\home\\Usuario\\DI\\sprint1tkinter\\catalog\\data\\edited\\th-3420979466.jpeg"),
-            Cell("F1 4", "C:\\msys64\\home\\Usuario\\DI\\sprint1tkinter\\catalog\\data\\edited\\wc1728204-2069878345.jpeg"),
-            Cell("F1 5", "C:\\msys64\\home\\Usuario\\DI\\sprint1tkinter\\catalog\\data\\edited\\wp6936516-653863971.jpeg"),
+            Cell("F1 1", "C:\\msys64\\home\\Usuario\\DI\\sprint1tkinter\\catalog\\data\\unedited\\mvwdc.jpeg"),
+            Cell("F1 2", "C:\\msys64\\home\\Usuario\\DI\\sprint1tkinter\\catalog\\data\\unedited\\th-828554261.jpeg"),
+            Cell("F1 3", "C:\\msys64\\home\\Usuario\\DI\\sprint1tkinter\\catalog\\data\\unedited\\th-3420979466.jpeg"),
+            Cell("F1 4", "C:\\msys64\\home\\Usuario\\DI\\sprint1tkinter\\catalog\\data\\unedited\\wc1728204-2069878345.jpeg"),
+            Cell("F1 5", "C:\\msys64\\home\\Usuario\\DI\\sprint1tkinter\\catalog\\data\\unedited\\wp6936516-653863971.jpeg"),
         ]
 
-        for i, cell in enumerate(self.cells):
-            label = ttk.Label(root,image=cell.image_tk,text=cell.title,compound= tk.BOTTOM)
+        #recorremos con el bucle for y mostramos al usuario cada imagen en funcion del label.grid
+        #Guardamos cada imagen en una variable para posteriormente poder redimensionarla. 
+        for i, cell in enumerate (self.cells):
+            img = Image.open(cell.path) #Abrimos una imagen en cada cell path con la biblioteca pillow.
+            img1 = img.resize((100,100),Image.LANCZOS) #redimensionamos la imagen y usamos el metodo image.LANCZOS para mejorar la calidad de redimensión
+            cell.image_tk = ImageTk.PhotoImage(img1) #Creamos un objeto con la imagen redimensionada
+            label = ttk.Label(root,image=cell.image_tk, text=cell.title,compound=tk.BOTTOM) #Creamos un widget
             label.grid(row=i,column=0)
-            label.bind("Button-1", lambda event, cell = cell: self.on_button_clicked)
