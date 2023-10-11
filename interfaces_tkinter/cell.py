91fe829ac88a9e4ac70ed843f0a24afd6957a195
Ivan Grovas <igrovasp22@fpcoruna.afundacion.org>
2023-10-11 09:32:41 +0200

    Creación de la carpeta recursos y con un JSON en su interior.

--- interfaces_tkinter/cell.py
+++
@@ -1,7 +0,0 @@
-from PIL import ImageTk
-
-class Cell:
-    def __init__(self,title,path) -> None:
-        self.title = title
-        self.path = path
-        self.image_tk = ImageTk.PhotoImage(file=self.path)
