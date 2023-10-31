644f5b74e5cfbdbf6fac6b88b87ab5e74ffc71e6
Iván Grovas <igrovasp22@fpcoruna.afundacion.org>
2023-10-31 09:45:56 +0100

    Completada tarea 3, Sprint 3 + Código de ejercicios anteriores comentado

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 
 public class CatalogActivity extends AppCompatActivity {
 
-    private Button button;
+    private Button button; //Añadimos el atributo privado de Button
     private Context context = this;
 
     @Override
@@ -18,10 +18,11 @@ public class CatalogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
 
-        button = findViewById(R.id.buttonAC);
-        button.setOnClickListener(new View.OnClickListener() {
+        button = findViewById(R.id.buttonAC); //Asociamos el Botton con el ID
+        button.setOnClickListener(new View.OnClickListener() { //Le asociamos una acción para cuando hagamos click en el botón
             @Override
             public void onClick(View view) {
+                //Iniciamos una nueva Actividad cada vez que se pulse en el botón
                 Intent intent = new Intent(context, DetailActivity.class);
                 context.startActivity(intent);
             }
